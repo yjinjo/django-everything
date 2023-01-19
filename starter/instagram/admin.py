58a554933b1d919c93a86211626951553616a15c
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-19 13:42:17 +0900

    #2. Media Files

--- starter/instagram/admin.py
+++ starter/instagram/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Post
 
 
@@ -7,6 +9,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         "id",
+        "photo_tag",
         "message",
         "message_length",
         "is_public",
@@ -16,3 +19,11 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ["message"]
     list_filter = ["created_at", "is_public"]
     search_fields = ["message"]
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f"<img src='{post.photo.url}' style='width: 72px;'>")
+        return None
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
--- starter/instagram/models.py
+++ starter/instagram/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to="instagram/post/%Y/%m/%d")
     is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,8 +12,3 @@ class Post(models.Model):
     # 객체에 대한 문자열 표현
     def __str__(self):
         return self.message
-
-    def message_length(self):
-        return len(self.message)
-
-    message_length.short_description = "메세지 글자수"
