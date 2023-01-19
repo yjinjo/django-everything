9a08111a16d1036f629e611082e4f66b32a022db
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-19 11:35:24 +0900

    #2. admin을 통한 데이터 관리

--- starter/instagram/admin.py
+++ starter/instagram/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "message",
+        "message_length",
+        "is_public",
+        "created_at",
+        "updated_at",
+    ]
+    list_display_links = ["message"]
+    list_filter = ["created_at", "is_public"]
+    search_fields = ["message"]
--- starter/instagram/models.py
+++ starter/instagram/models.py
@@ -4,5 +4,15 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # 객체에 대한 문자열 표현
+    def __str__(self):
+        return self.message
+
+    def message_length(self):
+        return len(self.message)
+
+    message_length.short_description = "메세지 글자수"
