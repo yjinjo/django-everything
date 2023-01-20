b408a001d1687046b5d9aa6f0dd315c08e919716
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-20 10:59:45 +0900

    #2. django-debug-toolbar를 통한 SQL 디버깅

--- starter/starter/settings.py
+++ starter/starter/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # third apps
     "django_extensions",
+    "debug_toolbar",
     # local apps
     "blog1",
     "instagram",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,6 +126,8 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
