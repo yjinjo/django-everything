ed9d14756155138a0898a013aa4df958f0846d65
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-02 16:10:47 +0900

    #4. Django Static

--- starter/starter/settings.py
+++ starter/starter/settings.py
@@ -128,7 +128,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = ""  # TODO
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # TODO
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "starter", "static")]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
