98d901e9afdddeb96af5fac4366b6e73cbc96870
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-03 16:46:56 +0900

    #6. 회원가입하자마자 로그인

--- starter/accounts/views.py
+++ starter/accounts/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, UpdateView, CreateView
 
@@ -47,13 +48,29 @@ def profile_edit(request):
     )
 
 
-User = get_user_model()
-signup = CreateView.as_view(
-    model=User,
-    form_class=UserCreationForm,
-    success_url=settings.LOGIN_URL,
-    template_name="accounts/signup_form.html",
-)
+# User = get_user_model()
+# signup = CreateView.as_view(
+#     model=User,
+#     form_class=UserCreationForm,
+#     success_url=settings.LOGIN_URL,
+#     template_name="accounts/signup_form.html",
+# )
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = "accounts/signup_form.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+        return response
+
+
+signup = SignupView.as_view()
 
 
 def logout(request):
