f805a7f2538898d903e7b5c886f0f07183b2ce69
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-03 09:55:42 +0900

    #5. ModelForm

--- starter/instagram/urls.py
+++ starter/instagram/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("new/", views.post_new, name="post_new"),
     path("<int:pk>", views.post_detail, name="post_detail"),
+    path("<int:pk>/edit/", views.post_edit, name="post_edit"),
     path("archive/", views.post_archive, name="post_archive"),
     path("archive/<int:year>/", views.post_archive_year, name="post_archive_year"),
 ]
--- starter/instagram/views.py
+++ starter/instagram/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView,
@@ -12,15 +14,38 @@ from .models import Post
 from .forms import PostForm
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save()
+            post = form.save(commit=False)
+            post.author = request.user  # 현재 로그인 User Instance
+            post.save()
             return redirect(post)
     else:
         form = PostForm()
-        
+
+    return render(request, "instagram/post_form.html", {"form": form})
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    # Tip: Decorator check
+    if post.author != request.user:
+        messages.error(request, "작성자만 수정할 수 있습니다.")
+        return redirect(post)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm(instance=post)
+
     return render(request, "instagram/post_form.html", {"form": form})
 
 
