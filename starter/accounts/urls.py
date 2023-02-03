54271a1a7ee847d026259c452ae5c1e8e42c4150
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-03 15:36:38 +0900

    #6. 사용자 프로필 페이지 및 프로필 수정

--- starter/accounts/urls.py
+++ starter/accounts/urls.py
@@ -1,1 +1,14 @@
-urlpatterns = []
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from accounts import views
+
+urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(template_name="accounts/login_form.html"),
+        name="login",
+    ),
+    path("profile/", views.profile, name="profile"),
+    path("profile/edit/", views.profile_edit, name="profile_edit"),
+]
